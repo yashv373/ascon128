ec5ecb02fd97b1bb264e2c8b52a6b060e6fd27b8
Yashvardhan Singh <149502221+yashv373@users.noreply.github.com>
2026-08-05 20:50:59 +0530

    Fix missing newline at end of tb_spi_slave.v
    Added a newline at the end of the file to comply with coding standards.

--- post_synthesis_GLS/ascon_full_GLS_postSYN/tb_spi_slave.v
+++ post_synthesis_GLS/ascon_full_GLS_postSYN/tb_spi_slave.v
@@ -73,6 +73,7 @@ module tb_spi_slave;
     task start_op(input dec, input adempty);
     begin
         wr(7'h20, {29'd0, adempty, dec, 1'b0});
+        repeat(5) @(posedge sclk);
         wr(7'h20, {29'd0, adempty, dec, 1'b1});
         wr(7'h20, {29'd0, adempty, dec, 1'b0});
     end endtask
